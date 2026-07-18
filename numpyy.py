80aa338ff3eaa6c5d18c4ae523e6681da3631f59
Rakshith B <rakshithb62@gmail.com>
2026-07-18 20:21:50 +0530

    hi

---
+++ numpyy.py
@@ -0,0 +1,8 @@
+import numpy as np
+
+# Print the installed version
+print("NumPy version:", np.__version__)
+
+# Create a simple NumPy array to prove it works
+my_array = np.array([10, 20, 30])
+print("My array looks like this:", my_array)
--- welcome.py
+++ welcome.py
@@ -1,2 +1,7 @@
 print('Hello World')
-print('Namaste Rakshith ')
+print('Namaste Rakshith')
+a=10
+b=20
+print(a+b)
+(a,b) = (b,a)
+print(a,b)
